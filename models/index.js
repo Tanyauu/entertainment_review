16f4884582f3d2cb69491a7ccf4f8c83267e48d0
moarshad100 <mo.arshad100@gmail.com>
2023-08-28 21:21:53 +1000

    made new relationships with the review to movie,game and tvshows using a foreignkeys for each

--- models/index.js
+++ models/index.js
@@ -2,3 +2,19 @@ const Game = require('./game');
 const Movie = require('./movie');
 const TV = require('./tvshows');
 const Review = require('./review');
+
+
+//movie has many reviews
+Movie.hasMany(Review,{
+	foreignKey:"movie_id"
+
+});
+
+Game.hasMany(Review,{
+	foreignKey:"game_id"
+
+});
+TV.hasMany(Review,{
+	foreignKey:"tv_id"
+
+});
