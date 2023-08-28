795e4d0a00961096643fd58b74503d8b28c82b7e
moarshad100 <mo.arshad100@gmail.com>
2023-08-28 22:06:01 +1000

    made minor changes

--- models/index.js
+++ models/index.js
@@ -1,20 +1,20 @@
-const Game = require('./game');
-const Movie = require('./movie');
+const Game = require('./games');
+const Movie = require('./movies');
 const TV = require('./tvshows');
 const Review = require('./review');
 
 
 //movie has many reviews
-Movie.hasMany(Review,{
-	foreignKey:"movie_id"
+Movie.hasMany(Review, {
+	foreignKey: "movie_id"
 
 });
 
-Game.hasMany(Review,{
-	foreignKey:"game_id"
+Game.hasMany(Review, {
+	foreignKey: "game_id"
 
 });
-TV.hasMany(Review,{
-	foreignKey:"tv_id"
+TV.hasMany(Review, {
+	foreignKey: "tv_id"
 
 });
