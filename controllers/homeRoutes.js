9014902ca20fc18223be9aa6769a47e6f8f31838
Renny <rennymwangi@gmail.com>
2023-08-31 23:59:03 +0930

    Remodel to the new model: Item

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,21 +1,26 @@
 const router = require('express').Router();
-const { Game, Movie, TV, Review } = require('../models');
+const { Item, Review } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    const gameData = await Game.findAll({
+    // const gameData = await Game.findAll({
+    //   include: [{ model: Review }],
+    // });
+    // const movieData = await Movie.findAll({
+    //   include: [{ model: Review }],
+    // });
+    // const tvData = await TV.findAll({
+    //   include: [{ model: Review }],
+    // });
+    const itemData = await Item.findAll({
       include: [{ model: Review }],
     });
-    const movieData = await Movie.findAll({
-      include: [{ model: Review }],
-    });
-    const tvData = await TV.findAll({
-      include: [{ model: Review }],
-    });
+
     res.render('homepage', {
-      gameData,
-      movieData,
-      tvData,
+      // gameData,
+      // movieData,
+      // tvData,
+      itemData,
     });
   } catch (error) {
     console.log(error);
