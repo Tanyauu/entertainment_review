be05f0536c5694e629f472155cce147241e4403b
Renny <rennymwangi@gmail.com>
2023-09-01 02:11:34 +0930

    Remodel user route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,12 @@ const gameRoutes = require('./game-routes');
 const movieRoutes = require('./movie-routes');
 const tvRoutes = require('./tv-routes');
 const reviewRoutes = require('./review-routes');
-// const userRoutes = require('./user-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/games', gameRoutes);
 router.use('/movies', movieRoutes);
 router.use('/tvs', tvRoutes);
 router.use('/reviews', reviewRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
--- controllers/api/user-routes/index.js
+++ controllers/api/user-routes/index.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../../models');
+
+// The `/api/users` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password', 'email'] },
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post('/', async (req, res) => {
   try {
@@ -39,10 +52,9 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
   } catch (err) {
     res.status(400).json(err);
   }
