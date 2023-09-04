b61a0916c6830122ee293e2d335295220d758394
moarshad100 <mo.arshad100@gmail.com>
2023-09-04 16:03:54 +1000

    added reviews

--- seeds/review-seeds.js
+++ seeds/review-seeds.js
@@ -64,13 +64,8 @@ const reviewData = [
         user_id:2,
 
     },
-    // {
-    //     text: "",
-    //     item_id:15,
-    //     rating: ,
-    //     user_id:  ,
+    // 
 
-    // },
     {
         text: "It’s not hard to get lost in the deep, subterranean world of Hollow Knight – and I mean that in more ways than one. ",
         item_id: 1 ,
@@ -113,177 +108,195 @@ const reviewData = [
         user_id: 1 ,
 
     },
-    // {
-    //     text: "",
-    //     item_id: 16,
-    //     rating: ,
-    //     user_id:  ,
+    {
+        text: "Really fun game with an amazing open world. The open world mechanics are probably some of the best I’ve ever seen.",
+        item_id: 16,
+        rating: 4 ,
+        user_id: 2,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 17,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "The story was well enough told. There were confusing parts when explaining certain characters and creators.",
+        item_id: 17,
+        rating: 3,
+        user_id: 3,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 18,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "Charming and Extraordinary As with previous Potter films, this one is wonderful. The mischievous trio are back in their third year at Hogwarts.",
+        item_id: 17,
+        rating: 5,
+        user_id: 1,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 19,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "How refreshing it is, then, to find that the just-concluded fourth season returns to the brilliant heights of its first.",
+        item_id: 18,
+        rating: 5 ,
+        user_id: 2,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id:  19,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "Okay this game could be great but right now for me it just alright.",
+        item_id: 19,
+        rating: 2,
+        user_id: 1,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 19 ,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "Fallout 4 is an amazing game. Featuring the most complex character creation system in gaming history",
+        item_id:  19,
+        rating: 5,
+        user_id: 2,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 20,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "Broken beyond all repair.",
+        item_id: 19 ,
+        rating: 1,
+        user_id: 3,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 21,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "the Fellowship of the Rings opened the door to a whole new world for me.",
+        item_id: 20,
+        rating: 5,
+        user_id: 1,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 22,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "I'd been having some difficulties in my life and discovered this amazing show on Netflix that helped me escape from the real world.",
+        item_id: 21,
+        rating: 5,
+        user_id: 2,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 23,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "This game is just plain fun. I have played over 40 hours so far and beyond an early bug where I couldn't finish a main storyline quest, it has been very smooth playing and very well done.",
+        item_id: 22,
+        rating: 4,
+        user_id: 2,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 24,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "They definitely needed at least 6-12 more months to actually finish the game and make it closer to what this could have been.",
+        item_id: 22,
+        rating: 2,
+        user_id: 3,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 24,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "The city is lifeless",
+        item_id: 22,
+        rating: 1,
+        user_id: 1,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 24,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "This movie really explores batman in a very different way. ",
+        item_id: 23,
+        rating: 5,
+        user_id: 1,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 24,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "From the first scene itself, it is made clear the amount of thought, work and creativity that has gone into the making of this movie.",
+        item_id: 23,
+        rating: 5,
+        user_id: 2,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 25,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "I am confused by entire ending.",
+        item_id: 23,
+        rating: 3,
+        user_id: 3,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 26,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: " When you look beyond the surface, it's also a great character study.",
+        item_id: 24,
+        rating: 5,
+        user_id: 1,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 27,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "There is so much that the first season of True Detective gets right",
+        item_id: 24,
+        rating: 4,
+        user_id: 2,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 28,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "It is compulsively watchable while providing a comfortable sense of the familiar and total immediacy.",
+        item_id: 24,
+        rating: 5,
+        user_id: 3,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id: 29,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "It is compulsively watchable while providing a comfortable sense of the familiar and total immediacy.",
+        item_id: 24,
+        rating: 4,
+        user_id: 4,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id:30,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "I found a newspaper a made a hat with it, 10/10 game,",
+        item_id: 25,
+        rating: 4,
+        user_id: 3,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id:30,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "Here's a film that is offensive only in how inoffensive it proves to be.",
+        item_id: 26,
+        rating: 2,
+        user_id: 1,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id:30,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "This series tackles all of the hard aspects of war, and doesn’t romanticize it. ",
+        item_id: 27,
+        rating: 5,
+        user_id: 2,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id:  ,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "Unending amount of game play if you pick a short span server.",
+        item_id: 28,
+        rating: 4,
+        user_id: 2,
 
-    // },
-    // {
-    //     text: "",
-    //     item_id:  ,
-    //     rating: ,
-    //     user_id:  ,
+    },
+    {
+        text: "Game hella raciest so it gets a 10/10",
+        item_id:28,
+        rating: 5,
+        user_id: 1,
+    },
 
-    // },
+    {
+        text: "Nostalgia!",
+        item_id: 29,
+        rating: 5,
+        user_id: 3,
 
+    },
+    {
+        text: "It is a well written tv show ",
+        item_id:30,
+        rating: 5,
+        user_id: 2,
 
+    },
+    {
+        text: "Absolute tops.",
+        item_id:30,
+        rating: 4,
+        user_id: 3,
 
+    },
 ];
 
 const seedReviews = () => Review.bulkCreate(reviewData);
