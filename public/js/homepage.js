57b92e7a066ff82be8aa5e806dcf7b89c459e7e3
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-09-05 01:17:00 +1000

    Fixed home page

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -8,14 +8,14 @@ const movieButtonHandler = async (event) => {
     });
 
     const moviedata = await response.json();
-    console.log(moviedata);
+    rkList.innerHTML = "";
     let sortedMovies = moviedata.sort((i1,i2) => (i1.reviews[0].rating < i2.reviews[0].rating) ? 1 : (i1.reviews[0].rating < i2.reviews[0].rating) ? -1 : 0);
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < 10; i++) {
       let li = document.createElement('li');
-      li.setAttribute("a", href=`/item/${sortedMovies.id}`);
+      li.setAttribute("href", `/api/items/${sortedMovies[i].id}`);
       li.setAttribute("class", "hover:text-yellow-400");
-      rkList.innerText = sortedMovies[i].name;
-      li.appendChild(rkList);
+      li.innerText = sortedMovies[i].name;
+      rkList.appendChild(li);
     }
     console.log(rkList);
   } catch (error) {
@@ -30,14 +30,14 @@ const tvButtonHandler = async (event) => {
     });
 
     const tvdata = await response.json();
+    rkList.innerHTML = "";
     let sortedTvs = tvdata.sort((i1,i2) => (i1.reviews[0].rating < i2.reviews[0].rating) ? 1 : (i1.reviews[0].rating < i2.reviews[0].rating) ? -1 : 0);
-    console.log(sortedTvs);
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < 10; i++) {
       let li = document.createElement('li');
-      li.setAttribute("a", href=`/item/${sortedTvs.id}`);
+      li.setAttribute("href", `/api/items/${sortedTvs[i].id}`);
       li.setAttribute("class", "hover:text-yellow-400");
-      rkList.innerText = sortedTvs[i].name;
-      li.appendChild(rkList);
+      li.innerText = sortedTvs[i].name;
+      rkList.appendChild(li);
     }
   } catch (error) {
     alert(response.statusText);
@@ -49,15 +49,16 @@ const gameButtonHandler = async (event) => {
     const response = await fetch(`/api/games`, {
       method: 'GET',
     });
-    console.log("hello3")
+
     const gamedata = await response.json();
+    rkList.innerHTML = "";
     let sortedGames = gamedata.sort((i1,i2) => (i1.reviews[0].rating < i2.reviews[0].rating) ? 1 : (i1.reviews[0].rating < i2.reviews[0].rating) ? -1 : 0);
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < 10; i++) {
       let li = document.createElement('li');
-      li.setAttribute("a", href=`/item/${sortedGames.id}`);
+      li.setAttribute("href", `/api/item/${sortedGames[i].id}`);
       li.setAttribute("class", "hover:text-yellow-400");
-      rkList.innerText = sortedGames[i].name;
-      li.appendChild(rkList);
+      li.innerText = sortedGames[i].name;
+      rkList.appendChild(li);
     }
   } catch (error) {
     alert(response.statusText);
@@ -71,15 +72,18 @@ async function fetchYear() {
     });
 
     const yeardata = await response.json();
+    yrList.innerHTML = "";
     // let sortedYears = yeardata.sort((i1,i2) => (i1.year < i2.year) ? 1 : (i1.year < i2.year) ? -1 : 0);
     let sortedYears = yeardata;
+    console.log(yeardata);
     for (i = 0; i < sortedYears.length; i++) {
       let li = document.createElement('li');
-      li.setAttribute("a", href=`/api/misc/year/${sortedYears.year}`);
+      li.setAttribute("href", `/api/misc/year/${sortedYears[i].year}`);
       li.setAttribute("class", "hover:text-yellow-400");
-      rkList.innerText = sortedYears[i].year;
-      li.appendChild(rkList);
+      li.innerText = sortedYears[i].year;
+      yrList.appendChild(li);
     }
+    console.log(yrList);
   } catch (error) {
     alert(response.statusText);
   }
