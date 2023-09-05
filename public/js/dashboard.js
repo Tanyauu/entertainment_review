7ad0c82d9cb8d3b72e09c12050e1c8cfd17592db
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-09-06 08:30:42 +1000

    updated Edit & Delete

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,15 @@
+editBtn = document.querySelectorAll('.review-form');
+deleteBtn = document.querySelectorAll('.delete-button');
+
 
 const editFormHandler = async (event) => {
   event.preventDefault();
-
-    const text = document.querySelector('#review-desc').value.trim();
-
-    const rating = document.querySelector('#review-desc').value;
+  // if (event.target.hasAttribute('data-id')) {    
+    console.log(event.target);
+    const id = event.target.getAttribute('data-id');
+    const text = document.getElementById('review').value.trim();
+    const rating = document.getElementById('rating').value;
+    console.log(id);
     console.log(text);
     console.log(rating);
     if (text && rating) {
@@ -15,13 +20,14 @@ const editFormHandler = async (event) => {
           'Content-Type': 'application/json',
         },
       });
-  
+
       if (response.ok) {
         document.location.replace('/dashboard');
       } else {
         alert('Failed to update review');
       }
     } 
+  // }
 };
 
 const delButtonHandler = async (event) => {
@@ -40,61 +46,11 @@ const delButtonHandler = async (event) => {
   }
 };
 
-// async function ratingRender() {
-//   try {
-//     const response = await fetch(`/api/reviews`, {
-//       method: 'GET',
-//     });
+editBtn.forEach(function(button) {
+  button.addEventListener('submit', editFormHandler);
+});
 
-//     const itemdata = await response.json();
-//     if (Math.floor(itemdata.rating) == 5) {
-//       star1.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star2.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star3.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star4.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star5.setAttribute("class", "w-4 h-4 text-yellow-300");
-//     } else if (Math.floor(itemdata.rating) ==4) {
-//       star1.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star2.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star3.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star4.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star5.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");     
-//     } else if (Math.floor(itemdata.rating) ==3) {
-//       star1.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star2.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star3.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star4.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star5.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");     
-//     } else if (Math.floor(itemdata.rating) ==2) {
-//       star1.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star2.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star3.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star4.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star5.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");     
-//     } else if (Math.floor(itemdata.rating) ==1) {
-//       star1.setAttribute("class", "w-4 h-4 text-yellow-300");
-//       star2.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star3.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star4.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star5.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");     
-//     } else {
-//       star1.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star2.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star3.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star4.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");
-//       star5.setAttribute("class", "w-4 h-4 text-gray-300 dark:text-gray-500");     
-//     }
-//   } catch (error) {
-//     alert(response.statusText);
-//   }
-// }
+deleteBtn.forEach(function(button) {
+  button.addEventListener('click', delButtonHandler);
+});
 
-document
-  .querySelector('#edit-review-form')
-  .addEventListener('submit', editFormHandler);
-
-document
-  .querySelector('.delete-review-form')
-  .addEventListener('click', delButtonHandler);
-
-// ratingRender();
