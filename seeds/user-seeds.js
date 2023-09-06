1f34c523fed858eef5934eefa5c1f2693367de9a
moarshad100 <mo.arshad100@gmail.com>
2023-09-06 23:09:40 +1000

    added more user-seeds and review-seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -28,7 +28,44 @@ const userData = [
     email: "ram@ent.com",
     password: "$2b$10$Ot8udr/mHnL.vBcY0EJs6.jNBcPP/4CAsMLiUOtBABmEasOntg.J2",
     //actualpassword:"passent";
-  }
+  },
+  {
+    name: "Harry",
+    email: "Harry@ent.com",
+    password: "$2b$10$dSw4mc0GcbrltHnDro12m.cmtVezaz8uq1Tu/4rK0HySJoFutD9VG",
+    //actualpassword:"RedStone";
+  },
+  {
+    name: "Eric",
+    email: "Eric@ent.com",
+    password: "$2b$10$bucmd863fCCCD8ckqnRNJuy0nBUoiIqha6bxRtp9XVj0PDOhsO/q6",
+    //actualpassword:"RoseBud";
+  },
+  {
+    name: "Emma",
+    email: "Emma@ent.com",
+    password: "$2b$10$vEAL/EJm7F140m6MJqSnyuc1s0AcCkGrOWwAP0T8Mi.Zz5I7Jkwjq",
+    //actualpassword:"Genocide";
+  },
+  {
+    name: "Pete",
+    email: "Pete@ent.com",
+    password: "$2b$10$16p6qk.pszVSnKX.GQGGieFAtXWt7Jz6uw1eEdcQs1zOuzTKb8GQC",
+    //actualpassword:"AlphaBeta";
+  },
+  {
+    name: "Solomon",
+    email: "Solomon@ent.com",
+    password: "$2b$10$RLkgL9HhrRIbdZ.VU6O38.DgncKrDTG8u8ltVKIBVe4MuB.x3CJ3a",
+    //actualpassword:"KingsCross";
+  },
+  {
+    name: "David",
+    email: "David@ent.com",
+    password: "$2b$10$ovh1YhgCjZlODKDUvwIuv.WDg8jTDm4SFHkqkHg1uH31bIycO.mS.",
+    //actualpassword:"DevilsCave";
+  },
+
 
 
 ];
